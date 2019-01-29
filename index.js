df97519dda6d57dcc36231bb6e39c6effc4229f6
adryson <47133111+adryson@users.noreply.github.com>
2019-01-29 01:56:06 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+var tmi = require('tmi.js');
+var options = 
+{
+	options: 
+	{
+		debug: true
+	},
+	connection: 
+	{
+		cluster: "aws",
+		reconnect: true
+	},
+	identity: 
+	{
+		username: "Sradryson",
+		password: "oauth:oauth:0a9o5qzf4olzwsss5wod3k4cnfbohw",  
+	},
+	channels: ['#johnpittertv', '#gaules',] //ADICIONA OS CANAIS AQ COM '#NOME'
+};
+
+var client = new tmi.client(options);
+
+	client.connect();
+	client.on("connected", function (address, port)
+	{
+		console.log("address: " + address + " port: " + port);
+		client.action("999mateus", "Hello 999mateus!")
+	});
